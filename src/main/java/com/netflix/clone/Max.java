d2adabe38489067d19a2aa85a9c075c02122bee3
prathamesh <prathameshwalekar9@gmail.com>
2026-06-07 11:27:43 +0530

    i am checking git and commiting s was the change

--- src/main/java/com/netflix/clone/Max.java
+++ src/main/java/com/netflix/clone/Max.java
@@ -4,7 +4,7 @@ public class Max {
     static class findMax{
         void add(int a,int b){
             System.out.println("two integers must not be dealt with ");
-            //hello gugz
+            //hello gugzs
         }
     }
 }
